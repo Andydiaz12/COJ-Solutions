28959d845733858e6a754868e96ad53c8bb8f6df
Andydiaz12 <jandrees12@gmail.com>
2017-03-14 10:42:17 -0600

    Add files via upload

--- 1753.cpp
+++ 1753.cpp
@@ -1,54 +1,51 @@
 #include <cstdio>
 #include <cstring>
+#include <cstdlib>
+
 using namespace std;
 
 int main(){
-	int casos, letras, i, l, z;
-	char palabra[20], arr[20], copia[20];
-	scanf("%d", &casos);
-	while(casos--){
-		z=0;
-		scanf("%s", palabra);
-		letras=strlen(palabra);
-		l=letras;
-		for(i=0; i<letras; i++){
-			if(palabra[i] == 'A' || palabra[i] == 'B' || palabra[i] == 'C' || palabra[i] == 'a' || palabra[i] == 'b' || palabra[i] == 'c')
-			arr[i]='2';
-			
-			else if(palabra[i] == 'D' || palabra[i] == 'E' || palabra[i] == 'F' || palabra[i] == 'd' || palabra[i] == 'e' || palabra[i] == 'f')
-			arr[i]='3';
-			
-			else if(palabra[i] == 'I' || palabra[i] == 'H' || palabra[i] == 'G' || palabra[i] == 'i' || palabra[i] == 'h' || palabra[i] == 'g')
-			arr[i]='4';
-			
-			else if(palabra[i] == 'J' || palabra[i] == 'K' || palabra[i] == 'L' || palabra[i] == 'j' || palabra[i] == 'k' || palabra[i] == 'l')
-			arr[i]='5';
-			
-			else if(palabra[i] == 'O' || palabra[i] == 'N' || palabra[i] == 'M' || palabra[i] == 'o' || palabra[i] == 'n' || palabra[i] == 'm')
-			arr[i]='6';
-			
-			else if(palabra[i] == 'P' || palabra[i] == 'Q' || palabra[i] == 'R' || palabra[i] == 'S' || palabra[i] == 'p' || palabra[i] == 'q' || palabra[i] == 'r' || palabra[i] == 's')
-			arr[i]='7';
-			
-			else if(palabra[i] == 'T' || palabra[i] == 'U' || palabra[i] == 'V' || palabra[i] == 'v' || palabra[i] == 'u' || palabra[i] == 't')
-			arr[i]='8';
-			
-			else if(palabra[i] == 'W' || palabra[i] == 'X' || palabra[i] == 'Y' || palabra[i] == 'Z' || palabra[i] == 'w' || palabra[i] == 'x' || palabra[i] == 'y' || palabra[i] == 'z')
-			arr[i]='9';
-		
-			copia[l-1]=arr[i];
-			l--;
-		}
-		for(i=0 ; i<letras; i++){
-			if(arr[i] != copia[i]){
-			z=1;
-			i=letras;
-		}
 
-		
-		}
-		(z == 1)?printf("N0\n"):printf("YES\n");
-	}
-	
-	return 0;
+    char letras[20];
+    int casos, size, i, aceptacion, tamano, copia[20];
+    
+    scanf("%d", &casos);
+    getchar();
+    
+    while(casos--){
+                   
+                   aceptacion=1;
+                   gets(letras);
+                   size = strlen(letras);
+                   for(i = 0 ; i < size ; i++){
+                        if(letras[i] == 'A' || letras[i] == 'B' || letras[i] == 'C' ||  letras[i] == 'a' || letras[i] == 'b' || letras[i] == 'c')
+                        copia[i] = 2;
+                        else if(letras[i] == 'D' || letras[i] == 'E' || letras[i] == 'F' ||  letras[i] == 'd' || letras[i] == 'e' || letras[i] == 'f')
+                        copia[i] = 3;
+                        else if(letras[i] == 'G' || letras[i] == 'H' || letras[i] == 'I' ||  letras[i] == 'g' || letras[i] == 'h' || letras[i] == 'i')
+                        copia[i] = 4;
+                        else if(letras[i] == 'J' || letras[i] == 'K' || letras[i] == 'L' ||  letras[i] == 'j' || letras[i] == 'k' || letras[i] == 'l')
+                        copia[i] = 5;
+                        else if(letras[i] == 'M' || letras[i] == 'N' || letras[i] == 'O' ||  letras[i] == 'm' || letras[i] == 'n' || letras[i] == 'o')
+                        copia[i] = 6;
+                        else if(letras[i] == 'P' || letras[i] == 'Q' || letras[i] == 'R' ||  letras[i] == 'p' || letras[i] == 'q' || letras[i] == 'r' || letras[i] == 'S' || letras[i] == 's')
+                        copia[i] = 7;
+                        else if(letras[i] == 'T' || letras[i] == 'U' || letras[i] == 'V' ||  letras[i] == 't' || letras[i] == 'u' || letras[i] == 'v')
+                        copia[i] = 8;
+                        else if(letras[i] == 'W' || letras[i] == 'X' || letras[i] == 'Y' ||  letras[i] == 'Z' || letras[i] == 'w' || letras[i] == 'y' || letras[i] == 'x' || letras[i] == 'z')
+                        copia[i] = 9;
+                   }
+       
+                   for(i = 0 ; i < size ; i++){
+                   if(copia[i] != copia[size-i-1]){
+                   aceptacion = 0;
+                   i = size;
+                   }
+                   }
+                   (aceptacion == 0) ? printf("NO\n") : printf("YES\n");
+                
+
+}
+
+    return 0;
 }
