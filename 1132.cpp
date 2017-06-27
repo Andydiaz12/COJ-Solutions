ebc18c942ce4fcd37beec3eb4193bdad2948f594
Andydiaz12 <jandrees12@gmail.com>
2017-06-26 19:15:46 -0500

    Add files via upload

--- 1132.cpp
+++ 1132.cpp
@@ -1,19 +1,27 @@
 #include <cstdio>
+#include <cmath>
+
 using namespace std;
 
 int main(){
-	int casos,i , suma, numero;
-	
-	scanf("%d", &casos);
-	while(casos--){
-		suma=0;
+
+	int casos, numero, suma, raiz, i;
+		scanf("%d", &casos);
+
+		while(casos--){
+			suma = 0;
 		scanf("%d", &numero);
-		for(i=1; i<numero; i++){
-			if(numero%i == 0)
-			suma+=i;
+
+		raiz = sqrt(numero);
+		for(i = 1 ; i <= raiz ; i++){
+			if(numero%i == 0){
+				suma+= i;
+				if(numero/i != i)
+				suma+= (numero/i);
+			}
 		}
-		printf("%d\n", suma);
-	}
-	
+		(numero == 1) ? printf("0\n") : printf("%d\n", suma-numero);
+
+		}
 	return 0;
 }
